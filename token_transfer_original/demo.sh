310c4ac4fe8a50e717aabb20e2cdc8b6d53928f1
Matiki <mattiikkii@gmail.com>
2024-08-21 22:57:20 +0200

    Refactor demo script

--- token_transfer_original/demo.sh
+++ token_transfer_original/demo.sh
@@ -1,48 +1,89 @@
 #!/usr/bin/env bash
+dfx killall
 dfx stop
 # set -e
 # trap 'dfx stop' EXIT
 
 echo "===========SETUP========="
 dfx start --background --clean
-dfx identity new alice_token_transfer --storage-mode plaintext --force
-export MINTER=$(dfx --identity anonymous identity get-principal)
-export DEFAULT=$(dfx identity get-principal)
-dfx deploy icrc1_ledger_canister --argument "(variant { Init =
-record {
+sleep 5
+
+deploy_ledger_canister() {
+  local initial_balance=$1
+  local minter=$2
+  local default=$3
+  dfx deploy icrc1_ledger_canister --argument "(variant { Init =
+  record {
      token_symbol = \"ICRC1\";
      token_name = \"L-ICRC1\";
-     minting_account = record { owner = principal \"${MINTER}\" };
-     transfer_fee = 10_000;
+     minting_account = record { owner = principal \"${minter}\" };
+     transfer_fee = 0_000;
      metadata = vec {};
-     initial_balances = vec { record { record { owner = principal \"${DEFAULT}\"; }; 10_000_000_000; }; };
+     initial_balances = vec { record { record { owner = principal \"${default}\"; }; ${initial_balance}; }; };
      archive_options = record {
-         num_blocks_to_archive = 1000;
-         trigger_threshold = 2000;
-         controller_id = principal \"${MINTER}\";
+       num_blocks_to_archive = 1000;
+       trigger_threshold = 2000;
+       controller_id = principal \"${minter}\";
      };
- }
-})"
-dfx canister call icrc1_ledger_canister icrc1_balance_of "(record {
-  owner = principal \"${DEFAULT}\";
-  }
-)"
-echo "===========SETUP DONE========="
+   }
+  })"
+}
+
+get_balance() {
+  local default=$1
+  result=$(dfx canister call icrc1_ledger_canister icrc1_balance_of "(record { owner = principal \"${default}\"; })")
+  echo $result
+}
+
+balances() {
+  echo "++++++++Balance of  default identity $DEFAULT_PRINCIPAL_ID" $(get_balance $DEFAULT_PRINCIPAL_ID)
+  echo "++++++++Balance of  Matiki identity $(dfx --identity Matiki identity get-principal)" $(get_balance $(dfx --identity Matiki identity get-principal))
+  echo "++++++++Balance of canister token_transfer_backend $(dfx canister id token_transfer_backend)" $(get_balance "$(dfx canister id token_transfer_backend)")
+}
+
+ledger_transfer_to() {
+    local to=$1
+    local amount=$2
+    dfx canister call icrc1_ledger_canister icrc1_transfer "(record {
+      to = record {
+        owner = principal \"${to}\";
+      };
+      amount = ${amount};
+    })"
+}
+
+canister_transfer_to() {
+    local amount=$1
+    local to_account=$2
+    dfx canister call token_transfer_backend transfer "(record {
+      amount = ${amount};
+      to_account = record {
+        owner = principal \"${to_account}\";
+      };
+    })"
+}
+
+dfx identity new alice_token_transfer --storage-mode plaintext --force
+export MINTER_PRINCIPAL_ID=$(dfx --identity anonymous identity get-principal)
+export DEFAULT_PRINCIPAL_ID=$(dfx identity get-principal)
+deploy_ledger_canister 10_000_000_000 $MINTER_PRINCIPAL_ID $DEFAULT_PRINCIPAL_ID
+
+echo "++++++++Balance of  default identity $DEFAULT_PRINCIPAL_ID" $(get_balance $DEFAULT_PRINCIPAL_ID)
 
 dfx deploy token_transfer_backend
+echo "===========SETUP DONE========="
+balances
 
-dfx canister call icrc1_ledger_canister icrc1_transfer "(record {
-  to = record {
-    owner = principal \"$(dfx canister id token_transfer_backend)\";
-  };
-  amount = 1_000_000_000;
-})"
+echo "===========ledger TRANSFER========="
+ledger_transfer_to "$(dfx canister id token_transfer_backend)" "9_000_000_000"
+balances
 
-dfx canister call token_transfer_backend transfer "(record {
-  amount = 100_000_000;
-  to_account = record {
-    owner = principal \"$(dfx identity get-principal)\";
-  };
-})"
+echo "===========canister TRANSFER========="
+canister_transfer_to 1_000_000_000 "$(dfx identity get-principal)"
+balances
+
+echo "===========canister TRANSFER========="
+canister_transfer_to 1_000_000_000 "$(dfx --identity Matiki identity get-principal)"
+balances
 
 echo "DONE"
