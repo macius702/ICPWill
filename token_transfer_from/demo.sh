f3ac7e2456e87e2b73410c6269093dedf9d74638
Matiki <mattiikkii@gmail.com>
2024-08-24 08:19:22 +0200

    Try on playground canister
    however Insufficient allowance again - why?

--- token_transfer_from/demo.sh
+++ token_transfer_from/demo.sh
@@ -8,90 +8,139 @@ dfx start --background --clean
 sleep 5
 dfx identity list
 
-export LEDGER=icrc1_ledger_canister
+# export LEDGER=icrc1_ledger_canister
+# export NETWORK=
+
+# export LEDGER=icrc1_ledger_canister
+# export NETWORK=
+export LEDGER=ryjl3-tyaaa-aaaaa-aaaba-cai
+export NETWORK=--ic
+
 
 function balance() {
     local identity=$1
-    dfx canister call $LEDGER icrc1_balance_of "(record {
+    # --identity Matiki is only to silence a warning about the identity 'default'
+    dfx canister $NETWORK call --identity Matiki $LEDGER icrc1_balance_of "(record {
       owner = principal \"$(dfx identity --identity $identity get-principal)\";
     })"
 }
 
 function balance_of_canister() {
   local canister_name=$1
-    dfx canister call $LEDGER icrc1_balance_of "(record {
+    dfx canister $NETWORK call $LEDGER icrc1_balance_of "(record {
       owner = principal \"$(dfx canister id $canister_name)\";
     })"
 }
 
 
 
-dfx deploy $LEDGER --argument "(variant {
-  Init = record {
-    token_symbol = \"ICRC1\";
-    token_name = \"L-ICRC1\";
-    minting_account = record {
-      owner = principal \"$(dfx identity --identity anonymous get-principal)\"
-    };
-    transfer_fee = 10_000;
-    metadata = vec {};
-    initial_balances = vec {
-      record {
-        record {
-          owner = principal \"$(dfx identity --identity Alice get-principal)\";
-        };
-        10_000_000_000;
-      };
-    };
-    archive_options = record {
-      num_blocks_to_archive = 1000;
-      trigger_threshold = 2000;
-      controller_id = principal \"$(dfx identity --identity anonymous get-principal)\";
-    };
-    feature_flags = opt record {
-      icrc2 = true;
-    };
-  }
-})"
+# dfx deploy $LEDGER --argument "(variant {
+#   Init = record {
+#     token_symbol = \"ICRC1\";
+#     token_name = \"L-ICRC1\";
+#     minting_account = record {
+#       owner = principal \"$(dfx identity --identity anonymous get-principal)\"
+#     };
+#     transfer_fee = 10_000;
+#     metadata = vec {};
+#     initial_balances = vec {
+#       record {
+#         record {
+#           owner = principal \"$(dfx identity --identity Alice get-principal)\";
+#         };
+#         10_000_000_000;
+#       };
+#     };
+#     archive_options = record {
+#       num_blocks_to_archive = 1000;
+#       trigger_threshold = 2000;
+#       controller_id = principal \"$(dfx identity --identity anonymous get-principal)\";
+#     };
+#     feature_flags = opt record {
+#       icrc2 = true;
+#     };
+#   }
+# })"
+
+balance Matiki
+MatikiBalanceBefore=$(balance Matiki)
+
 balance Alice
 balance Bob
 
 echo "===========SETUP DONE========="
 
-dfx deploy token_transfer_from_backend
+dfx deploy --playground token_transfer_from_backend
 balance Alice
+
+
+export BACKEND_CANISTER_ID=$(dfx canister --playground id token_transfer_from_backend)
+echo -e "\e[33mBACKEND_CANISTER_ID on playground: $BACKEND_CANISTER_ID\e[0m"
+
 
 echo "===========APPROVE========="
 # approve the token_transfer_from_backend canister to spend 100 tokens
-dfx canister call --identity Alice $LEDGER icrc2_approve "(
+dfx canister $NETWORK call --identity Alice $LEDGER icrc2_approve "(
   record {
     spender= record {
-      owner = principal \"$(dfx canister id token_transfer_from_backend)\";
+      owner = principal \"$BACKEND_CANISTER_ID\";
     };
-    amount = 10_000_000_000: nat;
+    amount = 1_000: nat;
   }
 )"
 
 balance Alice
 
-
-echo "===========TRANSFER========="
-dfx canister call token_transfer_from_backend transfer "(record {
-  amount = 100_000_000;
-  to_account = record {
-    owner = principal \"$(dfx identity --identity Bob get-principal)\";
-  };
-  from_account = record {
+echo "===========ALLOWANCE========="
+dfx canister $NETWORK call --identity Alice $LEDGER icrc2_allowance  "(record {
+  account = record {
     owner = principal \"$(dfx identity --identity Alice get-principal)\";
+  };
+  spender = record {
+    owner = principal \"$BACKEND_CANISTER_ID\";
   };
 })"
 
 balance Alice
+echo -e "\e[31mPerhaps this backend canister does not have enough cycles??\e[0m"
+
+
+
+echo "===========BACKEND CANISTER STATUS========="
+set -x
+dfx canister $NETWORK status $BACKEND_CANISTER_ID
+set +x
+
+
+echo "===========TRANSFER========="
+set -x
+dfx canister $NETWORK call $BACKEND_CANISTER_ID transfer "(record {
+  amount = 300;
+  to_account = record {
+    owner = principal \"$(dfx identity $NETWORK --identity Bob get-principal)\";
+  };
+  from_account = record {
+    owner = principal \"$(dfx identity $NETWORK --identity Alice get-principal)\";
+  };
+})"
+set +x
+balance Alice
 balance Bob
 
 
-balance_of_canister token_transfer_from_backend
 
+dfx canister $NETWORK call $LEDGER icrc1_balance_of "(record {
+  owner = principal \"$(dfx canister --playground id token_transfer_from_backend)\";
+})"
+
+balance Matiki
+
+MatikiBalanceAfter=$(balance Matiki)
+if [ "$MatikiBalanceBefore" == "$MatikiBalanceAfter" ]; then
+    echo "Matiki balance hasn't changed."
+else
+    echo "\e[31mMatiki balance has changed !!!\e[0m"
+fi
 
 
 echo "DONE"
