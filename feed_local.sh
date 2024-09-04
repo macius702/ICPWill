529b37a4f0c45104cf221f733c32e84feec8b6f1
Matiki <mattiikkii@gmail.com>
2024-09-04 17:01:22 +0200

    A helper script  to setup --network local

---
+++ feed_local.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+# feed --network=local identities
+
+# use identity that has much money 
+# dfx identity list
+# dfx identity use Alice
+dfx canister call mxzaz-hqaaa-aaaar-qaada-cai icrc1_transfer 
