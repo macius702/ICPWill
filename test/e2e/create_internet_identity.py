4da749eff8b9ba77456d183c87ad7ef35beaf8a3
Matiki <mattiikkii@gmail.com>
2024-09-17 15:34:38 +0200

    . Feed the 10000 identity with ICP

--- test/e2e/create_internet_identity.py
+++ test/e2e/create_internet_identity.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys  # Make sure this line is included
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
@@ -47,20 +47,12 @@ def create_with_feed():
     
     
     captcha_input = driver.find_element(By.ID, "captchaInput")
-    captcha_input.click()  # Click to ensure focus, although autofocus is set
-    captcha_input.send_keys('a' + Keys.ENTER)  # Type 'a'a', followed by pressing the Enter key
+    captcha_input.click()
+    captcha_input.send_keys('a' + Keys.ENTER)
     time.sleep(5)
     save_page_source(driver, 'page06c.html')
     
-    # # Locate the button by its ID and click it
-    # next_button = driver.find_element(By.ID, "confirmRegisterButton")
-    # next_button.click()
 
-
-    # time.sleep(5)
-    # save_page_source(driver, 'page07c.html')
-    
-    
     continue_button = driver.find_element(By.ID, "displayUserContinue")
     continue_button.click()
 
@@ -74,7 +66,7 @@ def create_with_feed():
     input_field = driver.find_element(By.CSS_SELECTOR, "input[placeholder='nick']")
     input_field.click()
 
-    # Optional: To type something in the input field
+
     input_field.send_keys("ExampleNickname")
 
     time.sleep(5)
@@ -98,140 +90,7 @@ def create_with_feed():
     subprocess.call(["./feed_local.sh", principal_value])
     
     print('DONE.')
-    
         
-    
-    
-
-
-def feed_with_money():
-    driver = create_driver()
-    url = "http://127.0.0.1:4943/?canisterId=be2us-64aaa-aaaaa-qaabq-cai"
-    driver.get(url)
-
-    save_page_source(driver, 'page03.html')
-    
-    click_element(driver, By.XPATH, "//button[text()='login']")
-    
-    time.sleep(10)
-    time.sleep(1)
-    
-    original_tab = driver.current_window_handle
-    driver.switch_to.window(driver.window_handles[-1])
-    time.sleep(5)
-
-    save_page_source(driver, 'page035.html')
-
-
-    use_existing_button = driver.find_element(By.ID, "loginButton")
-    use_existing_button.click()      
-    time.sleep(5)
-                
-    
-    
-    save_page_source(driver, 'page04.html')
-    
-    
-    add_new_device_button = driver.find_element(By.ID, "addNewDeviceButton")
-    add_new_device_button.click()    
-
-
-    time.sleep(5)
-    save_page_source(driver, 'page04b.html')
-    
-    
-    input_element = driver.find_element(By.CSS_SELECTOR, "input.c-input--vip")
-    print('input_element: ', input_element)
-    print("Tag Name:", input_element.tag_name)
-    print("Text:", input_element.text)
-    print("Class:", input_element.get_attribute('class'))
-    print("ID:", input_element.get_attribute('id'))
-    print("Placeholder:", input_element.get_attribute('placeholder'))
-
-    # Click on the input element
-    input_element.click()  # This will focus the input and simulate a click
-    time.sleep(5)
-    save_page_source(driver, 'page05b.html')
-
-    # Optionally, send keys if you need to simulate typing
-    input_element.send_keys("10000" + Keys.RETURN)  # Example to type '12345' and press Enter
-    time.sleep(5)
-    save_page_source(driver, 'page06b.html')
-    
-    
-    trust_device_button = driver.find_element(By.ID, "trustDeviceConfirm")
-    trust_device_button.click()    
-    time.sleep(10)
-    save_page_source(driver, 'page07b.html')
-    
-
-    driver.switch_to.window(original_tab)
-    
-    time.sleep(5)
-    save_page_source(driver, 'page08b.html')
-
-    
-    # input_field = driver.find_element(By.XPATH, "//input[@placeholder='Internet Identity']")
-    # print('input_field: ', input_field)
-    # identity_anchor = 10000
-    # identity_anchor = str(identity_anchor)
-    # input_field.send_keys(identity_anchor)
-    
-    # time.sleep(5)
-    
-    # save_page_source(driver, 'page05.html')
-    
-
-    # continue_button = WebDriverWait(driver, 10).until(
-    #     EC.element_to_be_clickable((By.XPATH, "//button[@data-action='continue']"))
-    # )
-    # continue_button.click()
-    
-    # driver.switch_to.window(original_tab)
-    
-    # time.sleep(5)
-    # save_page_source(driver, 'page06.html')
-    
-    
-    
-    
-
-def run():
-    driver = create_driver()
-    url = "http://br5f7-7uaaa-aaaaa-qaaca-cai.localhost:4943/#authorize"
-    driver.get(url)
-
-    try:
-        # Click the "Home" button
-        click_element(driver, By.XPATH, "//button[normalize-space()='Home']")
-
-        # Save elements and page source
-        print_elements(driver, file_path='elements.txt')
-        save_page_source(driver, 'page01.html')
-
-        # Click the "Register" button
-        click_element(driver, By.ID, "registerButton")
-
-        # Click the "Construct Identity" button
-        click_element(driver, By.CSS_SELECTOR, "button[data-action='construct-identity']")
-
-        # Input 'a' into the captcha input field
-        input_text(driver, By.ID, 'captchaInput', 'a')
-
-        # Click the "Confirm Register" button
-        click_element(driver, By.ID, 'confirmRegisterButton')
-
-        # Click the "Display User Continue" button
-        click_element(driver, By.ID, 'displayUserContinue')
-
-        # Save page source
-        save_page_source(driver, 'page02.html')
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        driver.quit()
-
 
 def click_element(driver, by_method, locator, timeout=10):
     try:
@@ -325,5 +184,3 @@ def create_driver():
 
 if __name__=="__main__":
     create_with_feed()
-#   run()
-#   feed_with_money()
