3b1297f85e481649892fa088f29dd98545602c08
Matiki <mattiikkii@gmail.com>
2024-10-11 08:05:56 +0200

    source export_local_env_vars.sh

--- build.sh
+++ build.sh
@@ -131,12 +131,7 @@ POP="\e[0m"
 
 
 if [ "$MODE" == "local" ]; then
-    export VITE_LEDGER_CANISTER_ID=mxzaz-hqaaa-aaaar-qaada-cai
-    export LEDGER=icrc1_ledger_canister
-    export VITE_IDENTITY_PROVIDER=http://br5f7-7uaaa-aaaaa-qaaca-cai.localhost:4943/
-    export VITE_NETWORK=
-    export PLAYGROUND=
-    export VITE_AGENT_HOST=http://127.0.0.1:4943
+  source export_local_env_vars.sh
 else
     export VITE_LEDGER_CANISTER_ID=ryjl3-tyaaa-aaaaa-aaaba-cai
     export LEDGER=$VITE_LEDGER_CANISTER_ID
---
+++ export_local_env_vars.sh
@@ -0,0 +1,6 @@
+export VITE_LEDGER_CANISTER_ID=mxzaz-hqaaa-aaaar-qaada-cai
+export LEDGER=icrc1_ledger_canister
+export VITE_IDENTITY_PROVIDER=http://br5f7-7uaaa-aaaaa-qaaca-cai.localhost:4943/
+export VITE_NETWORK=
+export PLAYGROUND=
+export VITE_AGENT_HOST=http://127.0.0.1:4943
