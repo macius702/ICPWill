27e28c19b18d88833dfa33fc763f89aa5d39294a
Matiki <mattiikkii@gmail.com>
2024-10-18 09:06:13 +0200

    . Add api test

--- test/api/test_api_icp.py
+++ test/api/test_api_icp.py
@@ -1,33 +1,48 @@
 import pytest
+import subprocess
 
+
+ICP_WILL_BACKEND = 'icp_will_backend'
 
 @pytest.fixture(scope="function")
 def setup_principals():
-    principals = []
+    return [
+        create_internet_identity('A1', with_icp_feed=True),
+        create_internet_identity('B2'),
+        create_internet_identity('C3')
+    ]
 
+def create_internet_identity(name, with_icp_feed=False):
 
+    subprocess.run(["dfx", "identity", "new", name, "--storage-mode=plaintext"], check=True)
+    
 
-    with open('principals.txt', 'rt') as f:
-        principals_read_back = [line.strip() for line in f]
-    print(principals_read_back)
+    principal = subprocess.check_output(["dfx", "identity", "--identity", name, "get-principal"], text=True).strip()
+    
 
-    for principal_str in principals_read_back:
-        Principal(principal_str)
-        principals.append(p)
+    if with_icp_feed:
+        subprocess.run(["./feed_local.sh", principal], check=True)
 
+    return Principal(name, principal)
 
 def test_send(setup_principals):
     principals = setup_principals
 
-    # read_balances
+    # Read balances
     for p in principals:
         print(p.get_balance())
 
+    assert True
 
 class Principal:
-    def __init__(self, principal_str):
-        self.name = principal_str
+    def __init__(self, name, principal_str):
+        self.name = name
+        self.principal = principal_str
+
     def get_balance(self):
-        dfx(
-
-
+        get_balance_returned = subprocess.check_output(
+            ["dfx", "canister", "call", ICP_WILL_BACKEND, "get_balance"],
+            text=True
+        ).strip()
+        print(get_balance_returned)
+        return get_balance_returned
